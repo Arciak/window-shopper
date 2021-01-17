d336866de80ceca23e260de01e0cf8a4ca9533b2
Artur Zarzecki <zarzecki.art@icloud.com>
2021-01-17 18:48:22 +0100

    Create custom text Field

---
+++ window-shopper/Views/CurrencyTxtField.swift
@@ -0,0 +1,35 @@
+//
+//  CurrencyTxtField.swift
+//  window-shopper
+//
+//  Created by Artur Zarzecki on 17/01/2021.
+//  Copyright © 2021 Artur Zarzecki. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTxtField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib() // when the app is call we start this code
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+//        if placeholder == nil {
+//            placeholder = " "
+//        }
+//
+//        let place = NSAttributedString(string: p, attributes: [.foregroundColor: UIColor.white])
+//        attributedPlaceholder = place
+//        textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        
+        // to u gory zastapilismytym u dolu, bardziej eleganckie
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: UIColor.white])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+
+}
