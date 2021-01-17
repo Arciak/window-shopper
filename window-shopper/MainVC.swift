36d5803e0f4636f1f74d3d40edd53b3dc840c20b
Artur Zarzecki <zarzecki.art@icloud.com>
2021-01-17 18:20:48 +0100

    Inicialize

---
+++ window-shopper/MainVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Artur Zarzecki on 17/01/2021.
+//  Copyright © 2021 Artur Zarzecki. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
