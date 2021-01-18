72a0b082ebe60f9cf520a67981e0c2e48747c7d7
Artur Zarzecki <zarzecki.art@icloud.com>
2021-01-18 14:06:10 +0100

    Add currency Lbl. POkazuje sie po lewej stronie Lbl fieldow

--- window-shopper/Views/CurrencyTxtField.swift
+++ window-shopper/Views/CurrencyTxtField.swift
@@ -12,6 +12,23 @@ import UIKit
 @IBDesignable
 class CurrencyTxtField: UITextField {
     
+    
+    
+    override func draw(_ rect: CGRect) { // nigdy nie implementowac tej funkcji jak jest pusta
+        let size: CGFloat = 20 // kiedy rysujemy potrzebujemy CGFloat
+        let currencyLbl = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2, width: size, height: size))
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.7270780457, green: 0.7270780457, blue: 0.7270780457, alpha: 0.8)
+        currencyLbl.textAlignment = .center
+        currencyLbl.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        currencyLbl.layer.cornerRadius = 5.0
+        currencyLbl.clipsToBounds = true //jeśli nie to to rogi nie beda zaokraglone
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLbl.text = formatter.currencySymbol
+        addSubview(currencyLbl)
+    }
+    
     // w tej funckji wpisujemy to co ma być widoczne w builderface view
     override func prepareForInterfaceBuilder() {
         customizeView()
@@ -36,6 +53,8 @@ class CurrencyTxtField: UITextField {
         //        textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
                 
                 // to u gory zastapilismytym u dolu, bardziej eleganckie
+        clipsToBounds = true //jeśli nie to to rogi nie beda zaokraglone
+        
         if let p = placeholder {
             let place = NSAttributedString(string: p, attributes: [.foregroundColor: UIColor.white])
             attributedPlaceholder = place
