ddfe87c012016a2a249e5209e1f1f5c88bd142a8
Artur Zarzecki <zarzecki.art@icloud.com>
2021-01-18 07:40:27 +0100

    Add Button above decimal kayboard

--- window-shopper/Controller/MainVC.swift
+++ window-shopper/Controller/MainVC.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class MainVC: UIViewController {
 
+    @IBOutlet weak var wageTxt: CurrencyTxtField!
+    @IBOutlet weak var priceTxt: CurrencyTxtField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // robimy UIButton w kodzie aby wyswietlał się przycisk nad klawiatura w apliakcji
+        
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calcBtn.backgroundColor = #colorLiteral(red: 0.8848350254, green: 0.5133160127, blue: 0.0113845669, alpha: 1)
+        calcBtn.setTitle("Calculate", for: .normal) // for normal state of the button, nie tam jakies nacisniecia
+        calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside) //#selector wola objective-C zobacz funckje calculate, ta metoda jest bezpieczniejsza jak np.: Selector("calculate") jesli funckja nie istnie apliakcje sie wykrzaczy
+        
+        wageTxt.inputAccessoryView = calcBtn
+        priceTxt.inputAccessoryView = calcBtn
+        
     }
-
-
+    
+    // kiedy nacisniemy kalkulator chcemy aby ta funckja zostala wywolana
+    @objc func calculate() {
+        print("We  got here")
+    }
 }
 
--- window-shopper/Views/CurrencyTxtField.swift
+++ window-shopper/Views/CurrencyTxtField.swift
@@ -8,28 +8,39 @@
 
 import UIKit
 
+
+@IBDesignable
 class CurrencyTxtField: UITextField {
+    
+    // w tej funckji wpisujemy to co ma być widoczne w builderface view
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
 
     override func awakeFromNib() {
         super.awakeFromNib() // when the app is call we start this code
+        customizeView()
+    }
+    
+    func customizeView() {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
         layer.cornerRadius = 5.0
         textAlignment = .center
-        
-//        if placeholder == nil {
-//            placeholder = " "
-//        }
-//
-//        let place = NSAttributedString(string: p, attributes: [.foregroundColor: UIColor.white])
-//        attributedPlaceholder = place
-//        textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-        
-        // to u gory zastapilismytym u dolu, bardziej eleganckie
+                
+        //        if placeholder == nil {
+        //            placeholder = " "
+        //        }
+        //
+        //        let place = NSAttributedString(string: p, attributes: [.foregroundColor: UIColor.white])
+        //        attributedPlaceholder = place
+        //        textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+                
+                // to u gory zastapilismytym u dolu, bardziej eleganckie
         if let p = placeholder {
             let place = NSAttributedString(string: p, attributes: [.foregroundColor: UIColor.white])
             attributedPlaceholder = place
             textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         }
     }
-
 }
+
